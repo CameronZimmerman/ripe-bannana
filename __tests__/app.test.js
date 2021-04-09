44826044a94055ab5e408b1685846cddf07117cd
Katrina Cloyd <Katrina.cloyd@yahoo.com>
2021-04-09 11:26:39 -0700

    test for actors post written

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -4,6 +4,27 @@ const db = require('../lib/utils/db.js');
 
 describe('ripe-bannana routes', () => {
   beforeEach(() => {
-    return db.sync({force: true})
+    return db.sync({ force: true })
   });
+
+  it('post adds an Actor to Actor table', () => {
+    return request(app)
+      .post('/api/v1/actors')
+      .send({
+        name: 'John John',
+        date_of_birth: 'Jan 1, 2020',
+        place_of_birth: 'Johnsville'
+      })
+      .then((res) => {
+        expect(res.body).toEqual({
+          id: 1,
+          name: 'John John',
+          date_of_birth: 'Jan 1, 2020',
+          place_of_birth: 'Johnsville'
+        })
+      })
+  })
+
+
+
 });
