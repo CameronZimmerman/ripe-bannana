762dc4cfaf38d47aa17e9b3ca8c39f477595d130
Katrina Cloyd <Katrina.cloyd@yahoo.com>
2021-04-09 11:49:14 -0700

    test written for actors get by id route

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,5 +1,6 @@
 const request = require('supertest');
 const app = require('../lib/app');
+const Actor = require('../lib/models/Actors');
 const db = require('../lib/utils/db.js');
 
 describe('ripe-bannana routes', () => {
@@ -15,6 +16,25 @@ describe('ripe-bannana routes', () => {
         dob: 'Jan 1, 2020',
         pob: 'Johnsville'
       })
+      .then((res) => {
+        expect(res.body).toEqual({
+          id: 1,
+          name: 'John John',
+          dob: expect.any(String),
+          pob: 'Johnsville'
+        })
+      })
+  })
+
+
+  it('get by id returns a scecific Actor by id', async () => {
+    await Actor.create({
+      name: 'John John',
+      dob: 'Jan 1, 2020',
+      pob: 'Johnsville'
+    })
+    return request(app)
+      .get('/api/v1/actors/1')
       .then((res) => {
         expect(res.body).toEqual({
           id: 1,
